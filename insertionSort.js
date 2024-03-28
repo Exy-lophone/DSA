e353d2578b595c69d9a5a824fbaa83b7fe2c094a
Exy <kevin.avdylaj02@gmail.com>
2024-03-28 15:03:12 +0100

    Added solution to addBinaryInteger exercise

--- insertionSort.js
+++ insertionSort.js
@@ -1,11 +1,11 @@
 const rndNbr = (from, to) => Math.floor(Math.random()* (to - from) + from)
 
 const toSort = [
-    Array(20).fill(1).map(() => rndNbr(0, 100)),
-    Array(20).fill(1).map(() => rndNbr(0, 100)),
-    Array(20).fill(1).map(() => rndNbr(0, 100)),
-    Array(20).fill(1).map(() => rndNbr(0, 100)),
-    Array(20).fill(1).map(() => rndNbr(0, 100)),
+    Array(21).fill(1).map(() => rndNbr(0, 100)),
+    Array(21).fill(1).map(() => rndNbr(0, 100)),
+    Array(21).fill(1).map(() => rndNbr(0, 100)),
+    Array(21).fill(1).map(() => rndNbr(0, 100)),
+    Array(21).fill(1).map(() => rndNbr(0, 100)),
 ]
 
 function insertionSort (toSort) {
@@ -23,4 +23,20 @@ function insertionSort (toSort) {
     return arr
 }
 
-console.log(toSort.map(x => ({unsorted: x, sorted: insertionSort(x)})))
+function insertionSortReverse (toSort) {
+    const arr = [...toSort]
+    for(let i = arr.length - 1; i >= 0; i--) {
+        const key = arr[i]
+        let j = i + 1
+        while (j < arr.length && arr[j] > key) {
+            arr[j - 1] = arr[j]
+            j++
+        }
+        arr[j - 1] = key
+    }
+
+    return arr
+}
+
+
+console.log(toSort.map(x => ({unsorted: x, sorted: insertionSortReverse(x)})))
